65ba28d014e5d454ddd5eacee423aa43d1402a08
OlcorOlcor <celovskyhonza@gmail.com>
2022-01-09 00:03:04 +0100

    Added 2 player mode

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import sys
 import os
 import re
-import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-
 
 """Displays the menu in console"""
 def DrawMenu():
@@ -23,7 +19,7 @@ def DrawMenu():
     print("#"+" "*14+"Press R for Controls"+" "*14+"#")
     print("#"+" "*48+"#")
     print("#"+" "*12+"Press T to quit the game"+" "*12+"#")
-    for i in range(4):
+    for i in range(2):
         print("#"+" "*48+"#")
     print("#"*50)
 
@@ -84,11 +80,32 @@ def StartOfGameLoop(Board):
         ResetStartOfGameMenu()
         User_Input = input("Enter what you want to do: ")
         if(User_Input == 'q' or User_Input == 'Q'): #starts a game for 2 player
-            print("Starting game for 2")
+            TwoPlayerGameLoop(Board)
         elif(User_Input == 'w' or User_Input == 'W'): #starts a game against AI
             print("Starting game against AI")
         elif(User_Input == 'e' or User_Input == 'E'): #returns to menu
             return
+
+"""Displays board in console"""
+def ResetBoard(Board):
+    clear = lambda: os.system('cls')
+    clear()
+    for r in Board:
+        print("".join(r))
+
+
+"""Function for taking input for two player game"""
+def TwoPlayerGameLoop(Board):
+    playing = True #boolean representing which player is about to make a move (X/O)
+    while True:
+        ResetBoard(Board)
+        if playing:
+            Board = PlayerInput(Board, "X's turn [X Y]: ", 'X')
+            playing = False #switches the player
+        else:
+            Board = PlayerInput(Board, "O's turn [X Y]: ", 'O')
+            playing = True #switcher the player
+
 
 
 """Creates and returns an array of arrays that represents the playing board"""
@@ -103,16 +120,20 @@ def CreatePlayingBoard(size: int):
 
 
 """Takes input from the user and checks whether or not its a valid move"""
-def PlayerInput(Board):
-    """Board: Array of arrays that represents the playing board"""
+def PlayerInput(Board: list, Message: str, Symbol: str):
+    """Board: Array of arrays that represents the playing board
+    Message: Message that'll be written in input
+    Symbol: Symbol that'll be added to the Board
+    """
     while True:
-        inp = input("Make a move [X Y]: ") #takes input from the user
+        inp = input(Message) #takes input from the user
         if(re.match("(\d|10) (\d|10)", inp)): #checks if its in a correct format
             coordinates = [int(i) for i in inp.split()] #splits the input into coordinates
             if(coordinates[0] in range(1, len(Board) + 1) and coordinates[1] in range(1, len(Board) + 1)): #checks if the coordinates aren't out of bounds
                 if(Board[coordinates[0] - 1][coordinates[1] - 1] == '.'): #checks if the tile isn't already filled
-                    Board[coordinates[0] - 1][coordinates[1] - 1] = 'X' #fills the square
-                    break
+                    Board[coordinates[0] - 1][coordinates[1] - 1] = Symbol #fills the square
+                    return Board
+        ResetBoard(Board)
 
 
 
