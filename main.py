5ab2c5188ce36904b065db8ea44cd20ff1ce111d
OlcorOlcor <celovskyhonza@gmail.com>
2022-01-08 15:42:55 +0100

    Added function for player input

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import re
 
 def DrawMenu():
     print("#"*50)
@@ -15,7 +15,9 @@ def DrawMenu():
     print("#"+" "*48+"#")
     print("#"+" "*13+"Press E for 10x10 game"+" "*13+"#")
     print("#"+" "*48+"#")
-    print("#"+" "*12+"Press R to quit the game"+" "*12+"#")
+    print("#"+" "*14+"Press R for Controls"+" "*14+"#")
+    print("#"+" "*48+"#")
+    print("#"+" "*12+"Press T to quit the game"+" "*12+"#")
     for i in range(4):
         print("#"+" "*48+"#")
     print("#"*50)
@@ -31,16 +33,39 @@ def MenuLoop():
     while True:
         User_Input = input("Enter what you want to do: ")
         if(User_Input == 'q' or User_Input == 'Q'):
-            print("Playing 3x3 game")
+            Board = CreatePlayingBoard(3)
+            PlayerInput(Board)
         elif(User_Input == 'w' or User_Input == 'W'):
-            print("Playing 5x5 game")
+            Board = CreatePlayingBoard(5)
         elif(User_Input == 'e' or User_Input == 'E'):
-            print("Playing 10x10 game")
+            Board = CreatePlayingBoard(10)
         elif(User_Input == 'r' or User_Input == 'R'):
+            print("Showing off controls")
+        elif(User_Input == 't' or User_Input == 'T'):
             sys.exit()
         else:
             ResetMenu()
         
+
+def CreatePlayingBoard(size):
+    Board = []
+    for r in range(size):
+        Board.append([])
+        for c in range(size):
+            Board[r].append('.')
+    return Board
+
+
+def PlayerInput(Board):
+    while True:
+        inp = input("Make a move [X Y]: ")
+        if(re.match("(\d|10) (\d|10)", inp)):
+            coordinates = [int(i) for i in inp.split()]
+            if(coordinates[0] in range(1, 4) and coordinates[1] in range(1, 4)):
+                if(Board[coordinates[0] - 1][coordinates[1] - 1] == '.'):
+                    Board[coordinates[0] - 1][coordinates[1] - 1] = 'X'
+                    break
+
 
 DrawMenu()
 MenuLoop()
